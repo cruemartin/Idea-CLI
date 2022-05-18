ee5bea5ec1207fd95be918df7b45fcd64553c93b
cruemartin <crue.c.martin@wmich.edu>
2022-05-18 12:10:28 -0400

    Got file reading/writing, flags, functinality working

---
+++ goIdeas.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Ideas struct {
+	Index int
+	Idea  string
+}
+
+func fileExist(path string) bool {
+	_, err := os.Stat(path)
+	return !errors.Is(err, os.ErrNotExist)
+}
+
+func writeToFile(path string, list *[]Ideas) {
+
+	bytes, err := json.Marshal(&list)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	err = ioutil.WriteFile(path, bytes, 0744)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+}
+
+func readFromFile(path string, list *[]Ideas) {
+	bytes, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	err = json.Unmarshal(bytes, list)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+func printList(list *[]Ideas) {
+
+	if len(*list) == 0 {
+		fmt.Println("Ideas List is empty...")
+	}
+
+	for i := 0; i < len(*list); i++ {
+		fmt.Printf("%d. %s\n", (*list)[i].Index, (*list)[i].Idea)
+	}
+}
+
+func removeFromList(index int, list *[]Ideas) *[]Ideas {
+	newList := make([]Ideas, 0)
+	newList = append(newList, (*list)[:index]...)
+	newList = append(newList, (*list)[index+1:]...)
+
+	for i := index; i < len(newList); i++ {
+		newList[i].Index = i + 1
+	}
+
+	return &newList
+
+}
+
+func editList(index int, text string, list *[]Ideas) {
+	(*list)[index].Idea = text
+}
+
+func main() {
+
+	var path string = "ideas.json"
+	var list []Ideas
+	var listPointer = &list
+	var write = true
+
+	viewcmd := flag.NewFlagSet("view", flag.ExitOnError)
+	newcmd := flag.NewFlagSet("new", flag.ExitOnError)
+	delcmd := flag.NewFlagSet("del", flag.ExitOnError)
+	editcmd := flag.NewFlagSet("edit", flag.ExitOnError)
+
+	idea := newcmd.String("I", "", "The idea to add to the Ideas List")
+	delIndex := delcmd.Int("i", -1, "The index of the idea to remove from Ideas List")
+	editIndex := editcmd.Int("i", -1, "The index of the ideas to edit")
+	editText := editcmd.String("t", "", "The text to replace")
+
+	//Check if list.json exist
+	if !fileExist(path) {
+		writeToFile(path, listPointer)
+	}
+
+	readFromFile(path, listPointer)
+
+	switch os.Args[1] {
+	case "view":
+		viewcmd.Parse(os.Args[2:])
+
+		write = false
+	case "new":
+		newcmd.Parse(os.Args[2:])
+		list = append(list, Ideas{len(list) + 1, *idea})
+		listPointer = &list
+	case "del":
+		delcmd.Parse(os.Args[2:])
+		if *delIndex == -1 {
+			fmt.Println("Invalid Index...")
+			return
+		}
+		listPointer = removeFromList(*delIndex-1, listPointer)
+	case "edit":
+		editcmd.Parse(os.Args[2:])
+		if *editIndex == -1 {
+			fmt.Println("Invalid Index...")
+		}
+		editList(*editIndex-1, *editText, listPointer)
+	}
+
+	printList(listPointer)
+
+	if write {
+		writeToFile(path, listPointer)
+	}
+
+}
